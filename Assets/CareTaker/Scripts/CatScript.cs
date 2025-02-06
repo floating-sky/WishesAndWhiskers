17fd2c069d2a00064085c703c5db9e56e9aee290
floating-sky <122412802+floating-sky@users.noreply.github.com>
2025-02-05 18:39:30 -0700

    The cat moves around the map randomly and will now meow at certain intervals when hungry or thirsty

--- Assets/CareTaker/Scripts/CatScript.cs
+++ Assets/CareTaker/Scripts/CatScript.cs
@@ -1,21 +1,104 @@
 using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class CatScript : MonoBehaviour
 {
+
+    [SerializeField]
+    public AudioSource hungryMeow;
+
+    [SerializeField]
+    public AudioSource thirstyMeow;
+
     public Boolean isHungry = false;
     public Boolean isThirsty = false;
+
+    // Cat is busy when they are doing an action that should not be interuptted
+    public Boolean isBusy = false;
+
+    // Cat is expressing a need through behavior. Used so that random movement doesn't interupt cat during behavior animation, but other actions CAN interupt
+    public Boolean isDoingBehavior = false;
+
+    public int moveRadius = 6; // How far from their current position the cat will move when they move to a random point
+    public int hungryMeowInterval = 15; // Seconds between each meow the cat makes when they are hungry
+    public int thirstyMeowInterval = 15;
+    public int randomMovementInterval = 6; // Seconds between each time the cat moves to a random location
+
+    NavMeshAgent agent;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+
+        InvokeRepeating("MoveCatToRandomDestination", 1, randomMovementInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+    // Grabs a random point from the walkable area (within the given radius)
+    public Vector3 GetRandomNavmeshLocation(float radius)
+    {
+        Vector3 randomDirection = UnityEngine.Random.insideUnitCircle * radius;
+        randomDirection += transform.position;
+        NavMeshHit hit;
+        Vector3 finalPosition = Vector3.zero;
+        if (NavMesh.SamplePosition(randomDirection, out hit, radius, 1))
+        {
+            finalPosition = hit.position;
+        }
+        return finalPosition;
+    }
+
+    // Called at each randomMovementInterval
+    public void MoveCatToRandomDestination() 
+    {
+        if (!isBusy && !isDoingBehavior)
+        {
+            agent.SetDestination(GetRandomNavmeshLocation(moveRadius));
+        }
+    }
+
+    public void CatHungryBehavior() 
+    {
+        if (!isBusy) 
+        {
+            isDoingBehavior = true;
+            agent.isStopped = true;
+            int secondsToWait = 3;
+            hungryMeow.Play();
+            StartCoroutine(WaitForAnimation(secondsToWait));
+            print("Cat is meowing (HUNGRY)");
+        }
+    }
+
+    public void CatThirstyBehavior() 
+    {
+        if (!isBusy)
+        {
+            isDoingBehavior = true;
+            agent.isStopped = true;
+            int secondsToWait = 3;
+            thirstyMeow.Play();
+            StartCoroutine(WaitForAnimation(secondsToWait));
+            print("Cat is meowing (THIRSTY)");
+        }
+    }
+
+    IEnumerator WaitForAnimation(int seconds) 
+    {
+        yield return new WaitForSeconds(seconds);
+        agent.isStopped = false;
+        isDoingBehavior = false;
     }
 
     public Boolean GetHungry()
@@ -25,9 +108,21 @@ public class CatScript : MonoBehaviour
 
     public void SetHungry(Boolean isHungry)
     {
+        if (this.isHungry == isHungry)
+            return;
         this.isHungry = isHungry;
-        print("Cat is Hungry");
+        if (isHungry)
+        {
+            print("Cat is Hungry");
+            InvokeRepeating("CatHungryBehavior", 1, hungryMeowInterval);
+        }
+        else
+        {
+            print("Cat is no longer hungry");
+            CancelInvoke("CatHungryBehavior");
+        }
     }
+
 
     public Boolean GetThirsty()
     {
@@ -36,7 +131,18 @@ public class CatScript : MonoBehaviour
 
     public void SetThirsty(Boolean isThirsty)
     {
+        if (this.isThirsty == isThirsty)
+            return;
         this.isThirsty = isThirsty;
-        print("Cat is Thirsty");
+        if (isThirsty)
+        {
+            print("Cat is Thirsty");
+            InvokeRepeating("CatThirstyBehavior", 1, thirstyMeowInterval);
+        }
+        else
+        {
+            print("Cat is no longer thirsty");
+            CancelInvoke("CatThirstyBehavior");
+        }
     }
 }

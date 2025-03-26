aee7f7c80fc6be58a6e96e3e8499999bbc416437
floating-sky <122412802+floating-sky@users.noreply.github.com>
2025-03-26 15:32:13 -0600

    Added washing frames

--- Assets/CareTaker/Scripts/CatInBathScript.cs
+++ Assets/CareTaker/Scripts/CatInBathScript.cs
@@ -1,11 +1,19 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CatInBathScript : MonoBehaviour
 {
     public BarScript washBarScript;
+    public Slider bar;
     private Boolean inTrigger = false;
     private Collider2D inputObject;
+
+    public Sprite frame1;
+    public Sprite frame2;
+    public Sprite frame3;
+    public Sprite frame4;
+    public Sprite frame5;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,6 +28,17 @@ public class CatInBathScript : MonoBehaviour
         {
             washBarScript.IncreaseProgressBar(0.001f);
         }
+
+        if (bar.value < .2)
+            GetComponent<SpriteRenderer>().sprite = frame1;
+        else if (bar.value < .4)
+            GetComponent<SpriteRenderer>().sprite = frame2;
+        else if (bar.value < .6)
+            GetComponent<SpriteRenderer>().sprite = frame3;
+        else if (bar.value < .8)
+            GetComponent<SpriteRenderer>().sprite = frame4;
+        else if (bar.value < 1)
+            GetComponent<SpriteRenderer>().sprite = frame5;
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
